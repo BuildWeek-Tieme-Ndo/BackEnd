2185c180b01c2b8a1e286d60cb0f2052d9320c79
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-18 13:12:11 -0600

    registration endpoint up and working

--- middleware/users-validation.js
+++ middleware/users-validation.js
@@ -1,34 +1,56 @@
 //import
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 //exports
 module.exports = {
-    validateUser,
-    getJwtToken
+  validateRegistration,
+  validateUser,
+  getJwtToken
 };
 
 //helper functions
-function validateUser(user) {
-    let errors = [];
-        !user.email
-            ? errors.push("Please provide an email address")
-            : !user.password
-                ? errors.push("Please provide a password")
-                : return { isSuccessful: errors.length > 0 ? false: true, errors};
+function validateRegistration(user) {
+  let errors = [];
+  if (!user.name) {
+    errors.push("Please provide a name for this user");
+  }
+  if (!user.email) {
+    errors.push("Please provide an email address");
+  }
+  if (!user.password) {
+    errors.push("Please provide a password");
+  }
+  return {
+    isSuccessful: errors.length > 0 ? false : true,
+    errors
+  };
+}
 
+function validateUser(user) {
+  let errors = [];
+  if (!user.email) {
+    errors.push("Please provide an email address");
+  }
+  if (!user.password) {
+    errors.push("Please provide a password");
+  }
+  return {
+    isSuccessful: errors.length > 0 ? false : true,
+    errors
+  };
 }
 
 function getJwtToken(email) {
-    const payload = {
-        email
-    };
-    const secret = {
-        process.env.JW_SECRET ||
-              "QB9DJvfhRWMwnJbbWkyaHjwf6Sp7T7DzPn4eC0fH897tXEz8thgrmHPURBFnMLSwE20tgk5YsXzyolrzsagPm3OXX9HmIfG0pxFEQVYSahnJeGRNnWpw0TyHXCQo07V8";
-    }
-    const options = {
-        expiresIn: '7d'
-    };
+  const payload = {
+    email
+  };
+  const secret =
+    process.env.JW_SECRET ||
+    "QB9DJvfhRWMwnJbbWkyaHjwf6Sp7T7DzPn4eC0fH897tXEz8thgrmHPURBFnMLSwE20tgk5YsXzyolrzsagPm3OXX9HmIfG0pxFEQVYSahnJeGRNnWpw0TyHXCQo07V8";
 
-    return jwt.sign(payload, secret, options);
+  const options = {
+    expiresIn: "7d"
+  };
+
+  return jwt.sign(payload, secret, options);
 }
--- routers/users-router.js
+++ routers/users-router.js
@@ -1,5 +1,37 @@
 //imports
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const db = require('../models/users-model');
 const restricted = require('../middleware/restricted-middleware');
+const { validateRegistration, validateUser, getJwtToken } = require('../middleware/users-validation');
 
+//endpoints for /api/
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const validateResult = validateRegistration(user);
+
+    if (validateResult.isSuccessful === true) {
+        const hash = bcrypt.hashSync(user.password, 14);
+        user.password = hash;
+
+        db.add(user)
+          .then(saved => {
+              res.status(201).json({ message: `user ${user.name} has been registered` });
+          })
+          .catch(err => {
+              console.log(err.toString());
+              res.status(500).json({ message: 'Could not register user; please try again later', error: err});
+          });
+    } else {
+        res.status(400).json({
+            message: "User info is invalid. See errors for details.",
+            errors: validateResult.errors
+        });
+    }
+});
+
+//exports
+module.exports = router;
