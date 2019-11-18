a58cee8ec68537001eaf7b8e2b9647bf267db11e
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-18 13:20:59 -0600

    login endpoint up and working

--- routers/users-router.js
+++ routers/users-router.js
@@ -33,5 +33,24 @@ router.post('/register', (req, res) => {
     }
 });
 
+router.post('/login', (req, res) => {
+    let { email, password } = req.body;
+
+    db.findBy({ email })
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+              const token = getJwtToken(user.email);
+              res.status(200).json({ message: `token delivery for ${user.name}!`, token});
+          } else {
+              res.status(401).json({ message: "Invalid Credentials" })
+          }
+      })
+      .catch(err => {
+          console.log(err.toString());
+          res.status(500).json({ message: 'Failed to log in. Please try again later', error: err})
+      })
+})
+
 //exports
 module.exports = router;
